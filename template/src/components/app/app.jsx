28fef9e27676b394d6e1a5acb41b39b2de131174
Bence A. Toth <5611525+bence-toth@users.noreply.github.com>
2020-03-29 16:17:44 +0200

    Fixed violations in app.jsx

--- template/src/components/app/app.jsx
+++ template/src/components/app/app.jsx
@@ -1,24 +1,27 @@
-import React from 'react';
-import './app.css';
+import React from 'react'
 
-function App() {
-  return (
-    <div className="app">
-      <header className="header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import './app.css'
 
-export default App;
+const App = () => (
+  <div className='app'>
+    <header className='header'>
+      <p>
+        Edit
+        {' '}
+        <code>src/App.js</code>
+        {' '}
+        and save to reload.
+      </p>
+      <a
+        className='link'
+        href='https://reactjs.org'
+        target='_blank'
+        rel='noopener noreferrer'
+      >
+        Learn React
+      </a>
+    </header>
+  </div>
+)
+
+export default App
